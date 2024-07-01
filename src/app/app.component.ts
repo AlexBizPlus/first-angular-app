aa925a1d6a6acd76586fcadd8a2e6ed5ee2cf554
Нежнов Алексей Вячеславович <anezhnov@nntc.pro>
2024-07-01 18:09:28 +0700

    [WIP]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { HeaderComponent } from './header/header.component';
+
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [UserComponent, HeaderComponent],
 })
 export class AppComponent {
   title = 'first-angular-app';
+  users = DUMMY_USERS;
+
+  onSelectUser(id: string) {
+    console.log(id);
+  }
 }
